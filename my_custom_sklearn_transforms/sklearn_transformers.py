b9fc0cb0d812adec3a78e2bfde49aec7659e58cf
alexandersw98 <70469919+alexandersw98@users.noreply.github.com>
2020-08-30 11:01:12 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,16 +1,46 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from tensorflow import keras
+from tensorflow.keras import regularizers
 
+# All sklearn Transforms must have the `transform` and `fit` methods    
+# Autoencoder
+class autocodificador(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        model=keras.Sequential()
+        # input 
+        #model.add(keras.Input(shape=(16,)))
+        
+        # codificacion
+        model.add(keras.layers.Dense(150, activation='tanh', activity_regularizer=regularizers.l1(10e-5)))
+        model.add(keras.layers.Dense(70, activation='relu'))
+        
+        # decodificacion
+        model.add(keras.layers.Dense(70, activation='tanh'))
+        model.add(keras.layers.Dense(100, activation='tanh'))
+        
+        # output
+        model.add(keras.layers.Dense(16,activation='relu'))
+        
+        # compile
+        model.compile(optimizer="adadelta", loss="mse")
+        
+        # instancia
+        self.autoencoder = model
+        
 
-# All sklearn Transforms must have the `transform` and `fit` methods
-class DropColumns(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
-        self.columns = columns
-
-    def fit(self, X, y=None):
+    def fit(self, X, y):
+        x_norm=X[y==0]
+        self.autoencoder.fit(x_norm[3000:5000], x_norm[3000:5000], batch_size = 256, epochs = 50, shuffle = True, validation_split = 0.20)
         return self
 
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+    def transform(self, X1):
+        # Aumentar la dimension de los datos para facilitar la deteccion de patrones
+        hidden_representation = keras.Sequential()
+        hidden_representation.add(self.autoencoder.layers[0])
+        hidden_representation.add(self.autoencoder.layers[1])
+        return hidden_representation.predict(X1)
+    
+    def fit_transform(self, X1, X2):
+        self.fit(X1,X2)
+        return self.transform(X1)
+    
